8428a7ca5bd923bd26e574526a2f84eb76bf7a7d
Farid Tabatabaie <farid.t@gmail.com>
2024-02-17 18:28:28 -0800

    Add some LED bar effects

--- code/2.4_ledBarGraph/2.4_ledBarGraph.ino
+++ code/2.4_ledBarGraph/2.4_ledBarGraph.ino
@@ -1,16 +1,48 @@
 void setup() {
-  for(int i=2;i<=11;i++)
+  for (int i=2; i<=11; i++)
   {
-    pinMode(i,OUTPUT);
+    pinMode(i, OUTPUT);
   }
 }
 
 void loop() {
-  for(int i=2;i<=11;i++)
-  {
-    digitalWrite(i,HIGH);
-    delay(500);
-    digitalWrite(i,LOW);
-    delay(500);    
+  for (int i=2; i<=11; i++) {
+    digitalWrite(i, HIGH);
+    delay(50);
+  }
+  for (int i=2; i<=11; i++) {
+    digitalWrite(i, LOW);
+    delay(50);
+  }
+
+  for (int i=11; i>=2; i--) {
+    digitalWrite(i, HIGH);
+    delay(50);
+  }
+  for (int i=11; i>=2; i--) {
+    digitalWrite(i, LOW);
+    delay(50);
+  }
+
+  for (int i=2; i<=6; i++) {
+    digitalWrite(i, HIGH);
+    digitalWrite(13-i, HIGH);
+    delay(100);
+  }
+  for (int i=2; i<=6; i++) {
+    digitalWrite(i, LOW);
+    digitalWrite(13-i, LOW);
+    delay(100);
+  }
+
+  for (int i=6; i>=0; i--) {
+    digitalWrite(i, HIGH);
+    digitalWrite(13-i, HIGH);
+    delay(100);
+  }
+  for (int i=6; i>=0; i--) {
+    digitalWrite(i, LOW);
+    digitalWrite(13-i, LOW);
+    delay(100);
   }
 }
