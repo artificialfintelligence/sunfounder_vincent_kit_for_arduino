598c2bd95650eaffa2dabeb36c63866f88611363
Farid Tabatabaie <farid.t@gmail.com>
2024-02-17 18:10:52 -0800

    Formatting

--- code/1.8_serialRead/1.8_serialRead.ino
+++ code/1.8_serialRead/1.8_serialRead.ino
@@ -2,15 +2,15 @@ const int ledPin = 9;
 int incomingByte = 0; 
 
 void setup() {
-  pinMode(ledPin,OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600); 
 }
 
 void loop() {
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
-    if(incomingByte=='1'){digitalWrite(ledPin,HIGH);}
-    else if(incomingByte=='0'){digitalWrite(ledPin,LOW);}
+    if (incomingByte == '1') {digitalWrite(ledPin, HIGH);}
+    else if(incomingByte == '0') {digitalWrite(ledPin, LOW);}
     
     //say what you got:
     Serial.print("I received: ");
