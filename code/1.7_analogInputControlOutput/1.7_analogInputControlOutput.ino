91ac76e6a72e8414a8f5115fd81127062be705ec
Farid Tabatabaie <farid.t@gmail.com>
2024-02-17 18:10:24 -0800

    Minor tweak to interpolation

--- code/1.7_analogInputControlOutput/1.7_analogInputControlOutput.ino
+++ code/1.7_analogInputControlOutput/1.7_analogInputControlOutput.ino
@@ -2,11 +2,11 @@ const int sensorPin = A0;
 const int ledPin = 9;      
 
 void setup() {
-  pinMode(ledPin,OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  int sensorValue=analogRead(sensorPin);
-  int brightness = map(sensorValue,0,1024,0,255);
-  analogWrite(ledPin,brightness);
+  int sensorValue = analogRead(sensorPin);
+  int brightness = map(sensorValue, 0, 1024, 0, 256);
+  analogWrite(ledPin, brightness);
 }
