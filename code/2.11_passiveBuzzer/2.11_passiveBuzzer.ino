780045a6855ac85c8db68f9847f58545883e0e67
Farid Tabatabaie <farid.t@gmail.com>
2024-02-25 16:29:53 -0800

    Add the second bar of melody

--- code/2.11_passiveBuzzer/2.11_passiveBuzzer.ino
+++ code/2.11_passiveBuzzer/2.11_passiveBuzzer.ino
@@ -1,23 +1,26 @@
 #include "pitches.h"
+
 int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4, 0, 
+  NOTE_F4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, 0, NOTE_E4, NOTE_F4, 0
 };
 
-int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
+int note_durations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4, 4, 
+  4, 8, 8, 4, 4, 4, 4, 4, 4
 };
 
 void setup() {
   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+  for (int this_note = 0; this_note < 18; this_note++) {
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000 / noteDurations[thisNote];
-    tone(9, melody[thisNote], noteDuration);
+    int note_duration = 1000 / note_durations[this_note];
+    tone(9, melody[this_note], note_duration);
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
+    int pause_between_notes = note_duration * 1.30;
+    delay(pause_between_notes);
     // stop the tone playing:
     noTone(9);
   }
